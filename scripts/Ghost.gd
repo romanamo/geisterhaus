8402c2cbfae278d6c4a8f664b20a387f8d0eeb1e
romanamo <r-schoene@gmx.net>
2024-05-04 21:00:46 +0200

    fixed ghost stopping bug

--- scripts/Ghost.gd
+++ scripts/Ghost.gd
@@ -19,19 +19,18 @@ func _process(delta):
 	var current_animation : String = self.animation
 	var sprite_size : Vector2 = self.sprite_frames.get_frame_texture(current_animation, 0).get_size()
 	
-	# Update speed
+	# Update speed according to pos and overlay size
 	if (position.x + sprite_size.x >= overlay_size.x) or (position.x <= 0):
 		speed.x = -speed.x
-		self.flip_h = !self.flip_h
+		self.flip_h = !self.flip_h # change sprite direction
 	if (position.y + sprite_size.y >= overlay_size.y) or (position.y <= 0):
 		speed.y = -speed.y
 	
+	# Retrieve sprite position on image
 	var sprite_center : Vector2 = self.position + sprite_size/2
+	var image_pos = overlay.mask_image.get_pixelv(Vector2i(sprite_center/overlay.scale)).a8
 	
-	print(self.position)
-	print(overlay.mask_image.get_pixelv(Vector2i(sprite_center)).a8)
-	
-	if overlay.mask_image.get_pixelv(Vector2i(sprite_center)).a8 == 0:
+	if image_pos == 0:
 		speed = Vector2.ZERO
 	
 	# Update position
