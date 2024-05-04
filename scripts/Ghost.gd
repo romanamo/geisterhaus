ecd02d50f46774da9ed540f8b46b63b602bbc4bb
romanamo <r-schoene@gmx.net>
2024-05-04 22:45:54 +0200

    added lookahead function

--- scripts/Ghost.gd
+++ scripts/Ghost.gd
@@ -6,6 +6,7 @@ extends AnimatedSprite2D
 
 # Ghost moving speed
 @export var speed : Vector2 = Vector2(2.5, 2.5)
+@export var SIGHT : float = 2.75
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,9 +16,17 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	# Retrieve current texture
-	
 	var current_animation : String = self.animation
 	var sprite_size : Vector2 = self.sprite_frames.get_frame_texture(current_animation, 0).get_size()
+	var sprite_center : Vector2 = self.position + sprite_size/2
+	
+	# Turn if not on black spots using calculated lookahead vector
+	var lookahead = (sprite_center + speed.normalized() * sprite_size * SIGHT).clamp(Vector2i.ZERO, get_window().size)
+	
+	if is_uncovered(Vector2(lookahead.x, position.y)):
+		speed.x *= -1
+	if is_uncovered(Vector2(position.x, lookahead.y)):
+		speed.y *= -1
 	
 	# Update speed according to pos and overlay size
 	if (position.x + sprite_size.x >= overlay_size.x) or (position.x <= 0):
@@ -26,14 +35,15 @@ func _process(delta):
 	if (position.y + sprite_size.y >= overlay_size.y) or (position.y <= 0):
 		speed.y = -speed.y
 	
-	# Retrieve sprite position on image
-	var sprite_center : Vector2 = self.position + sprite_size/2
-	var image_pos = overlay.mask_image.get_pixelv(Vector2i(sprite_center/overlay.scale)).a8
 	
-	if image_pos == 0:
+		
+	# Stop if player uncovers sprite
+	if is_uncovered(position):
 		speed = Vector2.ZERO
-	
 	# Update position
 	position += Vector2(speed.x, speed.y)
 	
 	
+
+func is_uncovered(pos: Vector2) -> bool:
+	return overlay.mask_image.get_pixelv(Vector2i(pos/overlay.scale)).a8 == 0
